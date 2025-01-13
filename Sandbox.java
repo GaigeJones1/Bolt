4e48b856c6f4d713454af3fabe6549e28bf52cc5
GaigeJones1 <Gaige.thomas.jones@gmail.com>
2025-01-13 08:20:45 -0500

    Added code to make the Sphere robot go forward and turn around

--- Sandbox.java
+++ Sandbox.java
@@ -3,3 +3,15 @@
 //Program: Sandbox
 //Hardware: Bolt #SB-2FAE
 
+async function startProgram() {
+	setMainLed({ r: 0, g: 0, b: 255 });
+	await speak("SandBox");
+    setSpeed(60);
+    await delay (3);
+    setSpeed(0);
+    await roll((getHeading() + 90), 60, 1);
+    setSpeed(60);
+    await delay (3);
+    setSpeed(0);
+    setMainLed({ r: 0, g: 0, b: 255});
+}
