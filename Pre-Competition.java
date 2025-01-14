1b5d17525c12678fde785aeb860d839890462ec0
GaigeJones1 <Gaige.thomas.jones@gmail.com>
2025-01-14 07:56:30 -0500

    Create Pre-Competition.java

---
+++ Pre-Competition.java
@@ -0,0 +1,22 @@
+//Team Members: Gaige Jones & Gage Hensley
+//Date: 1.14.2025
+//Program: Pre-Competition
+//Hardware: Bolt #SB-2FAE
+
+//Start
+//Bolt will say Start Acrossed the LED's
+//Driver: Gaige Jones
+//Navigator: Gage Hensley
+
+//red
+//bolt led will turn red
+//driver: Gage Hensley
+//Navigator: Gaige Jones
+
+//Forward & Turn 90 degrees and forward
+//driver: Gaige Jones
+//Navigator: Gage Hensley
+
+//play sound
+//driver: Gage Hensley
+//Navigator: Gaige Jones
