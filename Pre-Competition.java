0e1c76a0e6f56e2464fcdd06ba7a7980ce897065
GaigeJones1 <Gaige.thomas.jones@gmail.com>
2025-01-16 08:07:38 -0500

    Made a monkey sound happen

--- Pre-Competition.java
+++ Pre-Competition.java
@@ -1,3 +1,4 @@
+
 //Team Members: Gaige Jones & Gage Hensley
 //Date: 1.14.2025
 //Program: Pre-Competition
@@ -6,41 +7,50 @@
 //Start Program
 async function startProgram(){
 
-//Start & Say Start
-//Bolt will say Start Acrossed the LED's
-//Driver: Gaige Jones
-//Navigator: Gage Hensley
-
-    await speak("start", true);
+    //Start & Say Start
+    //Bolt will say Start Acrossed the LED's
+    //Driver: Gaige Jones
+    //Navigator: Gage Hensley
     
-    await delay(1);
-    await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true);
+      await speak("start", true);
+      await delay(1);
+      await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true);
     
+    //red
+    //bolt led will turn red
+    //driver: Gage Hensley
+    //Navigator: Gaige Jones
     
-//red
-//bolt led will turn red
-//driver: Gage Hensley
-//Navigator: Gaige Jones
+      await delay(1);
+      setMainLed({ r: 255, g: 0, b: 0 });
     
-    await delay(1);
-    setMainLed({ r: 255, g: 0, b: 0 });
+    //Forward & Turn 90 degrees and forward
+    //driver: Gaige Jones
+    //Navigator: Gage Hensley
     
-//Forward & Turn 90 degrees and forward
-//driver: Gaige Jones
-//Navigator: Gage Hensley
-
-    await delay(1);
-    setSpeed(60);
-    await delay(2)
-    setSpeed(0);
-    await roll((getHeading() + 90), 60, 1);
-    setSpeed(30);
-    await delay(1);
-    setSpeed(0);
-
-//play sound
-//driver: Gage Hensley
-//Navigator: Gaige Jones
-     
-//End Program
-}
+      await delay(1);
+      setSpeed(60);
+      await delay(2);
+      setSpeed(0);
+      await roll((getHeading() + 90), 60, 1);
+      setSpeed(30);
+      await delay(1);
+      setSpeed(0);
+    
+    //play sound
+    //driver: Gage Hensley
+    //Navigator: Gaige Jones
+    
+      await delay(1);
+      await Sound.Animal.Monkey.play(true);
+    
+    //Figure Eight
+    //Driver: Gaige Jones
+    //Navigator: Gage Hensley
+    
+    //Star
+    //driver: Gaige Jones
+    //Navigator: Gage Hensley
+    
+    //End Program
+    }
