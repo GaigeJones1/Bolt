9e27840bf80c6cb91b58bad6ee03f5e669d7514d
GaigeJones1 <Gaige.thomas.jones@gmail.com>
2025-01-24 08:12:06 -0500

    Start and say start

--- Maze.js
+++ Maze.js
@@ -10,18 +10,19 @@ async function startProgram(){
 //Bolt will say Start Acrossed the LED's & say it outloud
 //Driver: Gaige Jones
 //Navigator: Gage Hensley
+    await speak("start", true);
+    await delay(1);
+    await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true);
 
 //Green Checkpoint
 //LED's turn Green
 //Driver: Gage Hensley
 //Navigator: Gaige Jones
 
-
 //Sound #1 Checkpoint
 //Bolt makes sound of choice
 //Driver: Gaige Jones
 //Navigator: Gage Hensley
-
 
 //Red Checkpoint
 //LED's turn Red
