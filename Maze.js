33a7609197e8f14bf9d1659c6be8e56f572233ae
GaigeJones1 <Gaige.thomas.jones@gmail.com>
2025-01-24 08:01:16 -0500

    Made a plan

---
+++ Maze.js
@@ -0,0 +1,57 @@
+//Team Members: Gaige Jones & Gage Hensley
+//Date: 1.14.2025
+//Program: Pre-Competition
+//Hardware: Bolt #SB-2FAE
+
+//Start Program
+async function startProgram(){
+
+//Start & Say Start
+//Bolt will say Start Acrossed the LED's & say it outloud
+//Driver: Gaige Jones
+//Navigator: Gage Hensley
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Gage Hensley
+//Navigator: Gaige Jones
+
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice
+//Driver: Gaige Jones
+//Navigator: Gage Hensley
+
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Gage Hensley
+//Navigator: Gaige Jones
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - update sound
+//Driver: Gaige Jones
+//Navigator: Gage Hensley
+
+//Purple Checkpoint
+//LED's turn purple
+//Driver: Gage Hensley
+//Navigator: Gaige Jones
+
+//Sound #3 Checkpoint
+//Bolt makes sound of choice - update sound
+//Driver: Gaige Jones
+//Navigator: Gage Hensley
+
+//Blue Checkpoint
+//LED's turn blue
+//Driver: Gage Hensley
+//Navigator: Gaige Jones
+
+//Finish & Say finish
+//Bolt will say finished Acrossed the LED's & say it outloud
+//Driver: Gaige Jones
+//Navigator: Gage Hensley
+
+//End Program
+}
