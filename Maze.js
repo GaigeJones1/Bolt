03858aded2e7c2eac7bf1e0cdb44e95e11fa1b15
GaigeJones1 <Gaige.thomas.jones@gmail.com>
2025-01-29 08:31:55 -0500

    Purple Checkpoint

--- Maze.js
+++ Maze.js
@@ -56,25 +56,37 @@ async function startProgram(){
     //Bolt makes sound of choice - update sound
     //Driver: Gaige Jones
     //Navigator: Gage Hensley
-	await delay(1);
-	await roll((getHeading() - 90), 35, 1);
-	await delay(1);
-	setSpeed(35);
-	await delay(.3);
-	setSpeed(0);
-	await delay(1);
-	await roll((getHeading() - 87), 35, 1);
-	await delay(1);
-	setSpeed(35)
-	await delay(1.6)
-	setSpeed(0)
-	await Sound.Animal.Elephant.play(true);
-	
+    	await delay(1);
+    	await roll((getHeading() - 90), 35, 1);
+    	await delay(1);
+    	setSpeed(35);
+    	await delay(.3);
+    	setSpeed(0);
+    	await delay(1);
+    	await roll((getHeading() - 87), 35, 1);
+    	await delay(1);
+    	setSpeed(35)
+    	await delay(1.6)
+    	setSpeed(0)
+    	await Sound.Animal.Elephant.play(true);
+
     //Purple Checkpoint
     //LED's turn purple
     //Driver: Gage Hensley
     //Navigator: Gaige Jones
 
+      await delay(1);
+      await roll((getHeading() + 90), 35, 1);
+      await delay(1);
+      setSpeed(35);
+      await delay(1.5);
+      setSpeed(0);
+      await delay(1);
+      await roll((getHeading() + 45), 35, 1);
+      await delay(1);
+      setSpeed(35);
+      await delay(0.7);
+      setSpeed(0);
     //Sound #3 Checkpoint
     //Bolt makes sound of choice - update sound
     //Driver: Gaige Jones
